d14284f078614c6bbc6b5de1960dc0aa8ddac139
Runa <runa.uxlabs@gmail.com>
2024-03-14 17:22:25 +0900

    containers add & shadows, radii fix

---
+++ packages/chakra-ui-styled/src/foundation/shadows/shadows.types.ts
@@ -0,0 +1,12 @@
+export interface ShadowsType {
+  xs: string;
+  sm: string;
+  base: string;
+  md: string;
+  lg: string;
+  xl: string;
+  ["2xl"]: string;
+  ["dark-lg"]: string;
+  outline: string;
+  inner: string;
+}
