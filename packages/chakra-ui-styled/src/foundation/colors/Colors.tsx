881431d8bf971e3de2d6687cdc229bd0645398e9
smurf <smurf@kakao.com>
2024-03-28 17:34:35 +0900

    test

--- packages/chakra-ui-styled/src/foundation/colors/Colors.tsx
+++ packages/chakra-ui-styled/src/foundation/colors/Colors.tsx
@@ -29,8 +29,8 @@ const Colors = ({ colorCode, color, number }: colorProps) => {
 
   // color 이름이 black , white 일 경우 bgc 가 colorPalette.black.black;
   // 그 외 경우 colorPalette.색상[50]
-
-  let bgc = colorPalette[color as keyof typeof colorPalette][number as keyof (typeof colorPalette)[number]];
+  // testgit a
+  let bgc: string = colorPalette[color as keyof typeof colorPalette][number as keyof (typeof colorPalette)[number]];
   const ColorElement = styled.div`
     background-color: ${bgc};
 
