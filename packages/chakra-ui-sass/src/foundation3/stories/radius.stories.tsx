0fda93c192763b0939dda3ac8f3f4db2ef9302a5
강민협 <minhyup.kang@hanwha.com>
2024-03-31 18:18:50 +0900

    fix(sass): foundation 스토리북 파일 생성(foundation3)

---
+++ packages/chakra-ui-sass/src/foundation3/stories/radius.stories.tsx
@@ -0,0 +1,28 @@
+export default {
+  title: 'chakra-ui-sass/foundation'
+};
+
+const radiusKey = ['none', 'sm', 'base', 'lg', 'xl', '2xl', 'full'];
+
+export const Radius = () => {
+  return (
+    <div>
+      <div>Radius 입니다.</div>
+      <div style={{ display: 'flex', flexDirection: 'column', gap: '8px' }}>
+        {radiusKey.map((item) => {
+          return (
+            <div
+              key={`border-${item}`}
+              className={`border-radius-${item}`}
+              style={{ border: `var(--radius-${item}) solid blue` }}
+            >
+              Radius {item}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+Radius.storyName = 'Radius';
