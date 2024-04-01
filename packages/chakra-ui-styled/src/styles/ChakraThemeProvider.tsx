4a80943b656168d9963490f54311b042c0ca714c
bboing <gayoon109@gmail.com>
2024-04-01 16:28:54 +0900

    feat : theme bps, raddi, spacing, shadow add

--- packages/chakra-ui-styled/src/styles/ChakraThemeProvider.tsx
+++ packages/chakra-ui-styled/src/styles/ChakraThemeProvider.tsx
@@ -2,12 +2,36 @@ import { ThemeProvider } from 'styled-components';
 import { colorPalette2 } from '../foundation/colors/colorPalette';
 import { IColors } from '../foundation/colors/color.types';
 
+import Breakpoints from '../foundation/breakpoints/breakpoint';
+import { BreakpointsType } from '../foundation/breakpoints/breakpoint.types';
+
+import { Spacings } from '../foundation/spacings/spacing';
+import { SpacingsType } from '../foundation/spacings/spacing.types';
+
+import { Shadows } from '../foundation/shadows/shadow';
+import { ShadowsType } from '../foundation/shadows/shadows.types';
+
+import { RadiiType } from '../foundation/radii/radii.types';
+import { Radii } from '../foundation/radii/radii';
+
 export const ChakraThemeProvider = ({ children }: { children: React.ReactNode }) => {
   return (
     <ThemeProvider
       theme={{
         colors: {
           ...colorPalette2
+        },
+        breakpoint: {
+          ...Breakpoints
+        },
+        spacing: {
+          ...Spacings
+        },
+        shadow: {
+          ...Shadows
+        },
+        radii: {
+          ...Radii
         }
       }}
     >
@@ -19,5 +43,9 @@ export const ChakraThemeProvider = ({ children }: { children: React.ReactNode })
 declare module 'styled-components' {
   export interface DefaultTheme {
     colors: IColors;
+    breakpoint: BreakpointsType;
+    spacing: SpacingsType;
+    shadow: ShadowsType;
+    radii: RadiiType;
   }
 }
