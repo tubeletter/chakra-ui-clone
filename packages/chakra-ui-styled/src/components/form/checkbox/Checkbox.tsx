49e68afec9f0891f46de222c8875bc3d2ab52c1a
bboing <gayoon109@gmail.com>
2024-04-08 09:53:26 +0900

    fix : 체크박스 수정

--- packages/chakra-ui-styled/src/components/form/checkbox/Checkbox.tsx
+++ packages/chakra-ui-styled/src/components/form/checkbox/Checkbox.tsx
@@ -9,8 +9,8 @@ export interface CheckboxProps {
   // Indeterminate는 체크를 했다는 전제조건에서만 나올 수 있음, isDisabled가 true이면 Indeterminate는 자동으로 해제, 우선 배제하고 작업진행
   text: string;
 }
-const checkboxSize = { sm: 12, md: 16, lg: 20, };
-const iconSize = { sm: 8, md: 12, lg: 16, };
+const checkboxSize = { sm: 12, md: 16, lg: 20 };
+const iconSize = { sm: 8, md: 12, lg: 16 };
 // 기본색상
 const getColor = (colorScheme: string) => css`
   ${({ theme }) => {
@@ -49,13 +49,13 @@ const getLabelPadding = (size: string) => css`
     }
   }}
 `;
-const CheckboxForm = styled.input.attrs({ type: "checkbox" })`
+const CheckboxForm = styled.input.attrs({ type: 'checkbox' })`
   position: absolute;
   opacity: 0;
   cursor: pointer;
   height: 0;
   width: 0;
-  
+
   &:checked ~ span::after {
     display: flex;
   }
@@ -82,7 +82,7 @@ const Label = styled.label<CheckboxProps>`
     border-radius: 2px;
     text-align: center;
 
-    &::after { 
+    &::after {
       position: absolute;
       font-size: ${({ size }) => iconSize[size]}px;
       width: inherit;
@@ -95,80 +95,60 @@ const Label = styled.label<CheckboxProps>`
       align-items: center;
     }
 
-    ${({ isChecked, isDisabled, isIndeterminate, colorScheme, theme }) => {
-    switch (true) {
-      case (isChecked == true):
-        if (isDisabled) { // 체크되고 비활성화
-          // if (isIndeterminate) { // isIndeterminate 설정되면 isDisabled = false;로 바꾸고 -뜨게하는 법?
-          //   isDisabled = false;
-          //   return
-          // } 
-          return css`
-              background-color: ${theme.color.gray[200]};
-              border: none;
+    ${({ isChecked, isDisabled, isIndeterminate, colorScheme, theme }) => css`
+      ${isDisabled
+        ? css`
+            background-color: ${theme.color.gray[200]};
+            border: none;
+            ${isChecked &&
+            css`
               &::after {
                 content: '✔';
                 color: ${theme.color.gray[500]};
               }
-            `;
-        } else { // 체크되고 활성화
-          if (isIndeterminate) { // isIndeterminate 설정되면 -아이콘
-            return css`
+            `}
+          `
+        : isChecked
+          ? css`
               background-color: ${getColor(colorScheme)};
               border: 1px solid ${getColor(colorScheme)};
               &::after {
-                content: '-';
+                ${isIndeterminate
+                  ? css`
+                      content: '-';
+                      transform: translate(-50%, -60%);
+                    `
+                  : css`
+                      content: '✔';
+                    `}
                 color: ${theme.color.white.white};
                 transform: translate(-50%, -60%);
               }
-            `;
-          }
-          return css`
-              background-color: ${getColor(colorScheme)};
-              border: 1px solid ${getColor(colorScheme)};
-              &::after {
-                content: '✔';
-                color: ${theme.color.white.white};
-              }
-            `;
-        };
-      case (isChecked == false):
-        if (isDisabled) {// 체크안되고 비활성화
-          return css`
-              background-color: ${theme.color.gray[100]};
-              border: none;
-              &::after {
-                content: '';
-              }
-            `;
-        } else { // 체크안되고 활성화
-          // if (isIndeterminate) { // isIndeterminate 설정되면 isDisabled = true;로 바꾸고 -뜨게하는 법?
-          //   isChecked = true;
-          //   return css`
-          //     background-color: ${getColor(colorScheme)};
-          //     border: 1px solid ${getColor(colorScheme)};
-          //     &::after {
-          //       content: '-';
-          //       color: ${theme.color.white.white};
-          //       transform: translate(-50%, -60%);
-          //     }
-          //   `;
-          // }
-          return css`
-              background-color: none;
+            `
+          : css`
               border: 2px solid ${theme.color.gray[200]};
-              &::after {
-                content: '';
-              }
-            `;
-        }
-    }
+              background: none;
+            `}
+    `}
   }
-  }}
 `;
-const Checkbox = ({ size = 'md', isChecked = true, isDisabled = false, isIndeterminate = false, colorScheme, text }: CheckboxProps) => {
+const Checkbox = ({
+  size = 'md',
+  isChecked = true,
+  isDisabled = false,
+  isIndeterminate = false,
+  colorScheme,
+  text
+}: CheckboxProps) => {
   return (
-    <Label size={size} colorScheme={colorScheme} isChecked={isChecked} isDisabled={isDisabled} isIndeterminate={isIndeterminate} text="" >
+    <Label
+      size={size}
+      colorScheme={colorScheme}
+      isChecked={isChecked}
+      isDisabled={isDisabled}
+      isIndeterminate={isIndeterminate}
+      text=""
+    >
       <CheckboxForm />
       <span />
       {text}
