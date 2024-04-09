37639c7baa9f83cc1ee27374c610a51a65e69558
bboing <gayoon109@gmail.com>
2024-04-09 16:12:09 +0900

    feat : foundation typo add.

--- packages/chakra-ui-styled/src/foundation/typography/Heading.tsx
+++ packages/chakra-ui-styled/src/foundation/typography/Heading.tsx
@@ -1,14 +1,33 @@
-import { HeadingProps } from './typography.types';
-import * as S from './Heading.styled';
+import styled, { css } from 'styled-components';
 
-export const Heading = ({ as, value, size }: HeadingProps) => {
-  const text = 'In love with Chakra 〉';
+export interface HeadingProps {
+  as?: React.ElementType;
+  children?: React.ReactNode;
+  size: '4xl' | '3xl' | '2xl' | 'xl' | 'lg' | 'md' | 'sm' | 'xs';
+}
+
+const StyleHeading = styled.h1<HeadingProps>`
+  ${({ theme, size }) => css`
+    ${theme.typo.fontFamily};
+    ${theme.breakpoint.base} {
+      font-size: ${theme.typo.small[size].fontSize};
+      font-weight: ${theme.typo.small[size].fontWeight};
+      line-height: ${theme.typo.small[size].lineHeight};
+    }
+    ${theme.breakpoint.lg} {
+      font-size: ${theme.typo.large[size].fontSize};
+      font-weight: ${theme.typo.large[size].fontWeight};
+      line-height: ${theme.typo.large[size].lineHeight};
+    }
+  `};
+`;
+
+export const Heading = ({ as, size, children }: HeadingProps) => {
   return (
     <>
-      <p>{value}</p>
-      <S.Heading value={value} size={size} as={as}>
-        {text}
-      </S.Heading>
+      <StyleHeading as={as} size={size}>
+        {children}
+      </StyleHeading>
     </>
   );
 };
--- packages/chakra-ui-styled/src/foundation/typography/Texts.stories.tsx
+++ packages/chakra-ui-styled/src/foundation/typography/Texts.stories.tsx
@@ -1,25 +1,49 @@
-import { TextComponent } from './Text';
+import styled from 'styled-components';
+import { TextComponent, TextProps } from './Text';
 
 export default {
   title: 'chakra-ui-styled/foundation/typography/Text',
-  component: Text,
-  argTypes: {},
-  parameter: {}
+  component: TextComponent,
+  parameters: { controls: { expanded: true } },
+  argTypes: {
+    children: { control: { type: 'text' } },
+    size: { control: { type: 'select' } },
+    weight: { control: { type: 'select' } }
+  },
+  args: {
+    children: '텍스트 컴포넌트 args 테스트',
+    size: 'md'
+  }
 };
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
 
-export const Texts = () => {
+export const Texts = (args: TextProps) => {
+  const text = 'In love with Chakra';
   return (
-    <div>
-      <TextComponent value="6xl" />
-      <TextComponent value="5xl" />
-      <TextComponent value="4xl" />
-      <TextComponent value="3xl" />
-      <TextComponent value="2xl" />
-      <TextComponent value="xl" />
-      <TextComponent value="lg" />
-      <TextComponent value="md" />
-      <TextComponent value="sm" />
-      <TextComponent value="xs" />
-    </div>
+    <Wrapper>
+      <h1>Texts</h1>
+
+      <h2>1. Texts Demo</h2>
+      <div>
+        <TextComponent {...args} />
+      </div>
+      <h2>2. Texts Size Demo</h2>
+      <div>
+        <TextComponent size="6xl" children={text} />
+        <TextComponent size="5xl" children={text} />
+        <TextComponent size="4xl" children={text} />
+        <TextComponent size="3xl" children={text} />
+        <TextComponent size="2xl" children={text} />
+        <TextComponent size="xl" children={text} />
+        <TextComponent size="lg" children={text} />
+        <TextComponent size="md" children={text} />
+        <TextComponent size="sm" children={text} />
+        <TextComponent size="xs" children={text} />
+      </div>
+    </Wrapper>
   );
 };
