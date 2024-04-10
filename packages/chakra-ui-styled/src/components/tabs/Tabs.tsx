179228a108881955af465d5e189e3439b612b9c7
smurf <smurf@kakao.com>
2024-04-10 15:24:40 +0900

    add : tab tabs first init

---
+++ packages/chakra-ui-styled/src/components/tabs/Tabs.tsx
@@ -0,0 +1,20 @@
+const Tabs = () => {
+  return <></>;
+};
+
+// tab atoms 종류
+//
+// unstyled
+// soft-rounded
+// solid-rounded
+// enclosed
+// line
+//
+// tab 사이즈
+//
+// lg
+// md
+// sm
+// tabs 종류별 1묶음으로 크기 분류
+
+export default Tabs;
