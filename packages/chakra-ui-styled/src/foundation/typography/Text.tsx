6c969e73eb5d54e5231e6baa276d495d5928110e
bboing <gayoon109@gmail.com>
2024-04-16 09:12:05 +0900

    fix : typo setting

--- packages/chakra-ui-styled/src/foundation/typography/Text.tsx
+++ packages/chakra-ui-styled/src/foundation/typography/Text.tsx
@@ -1,42 +1,17 @@
-import styled, { css } from 'styled-components';
+import * as S from './Text.styled';
 
 export interface TextProps {
   children?: React.ReactNode;
   size: '6xl' | '5xl' | '4xl' | '3xl' | '2xl' | 'xl' | 'lg' | 'md' | 'sm' | 'xs';
   weight?: 'bold' | 'semibold' | 'medium' | 'normal';
 }
-const getFontWeight = (weight: string) => css`
-  ${({ theme }) => {
-    switch (weight) {
-      case 'bold':
-        return theme.typo.fontWeight.bold;
-      case 'semibold':
-        return theme.typo.fontWeight.semibold;
-      case 'medium':
-        return theme.typo.fontWeight.medium;
-      case 'normal':
-        return theme.typo.fontWeight.normal;
-      default:
-        theme.typo.fontWeight.normal;
-    }
-  }}
-`;
-
-export const StyleText = styled.p<TextProps>`
-  ${({ theme, size, weight }) => css`
-    ${theme.typo.fontFamily};
-    font-size: ${theme.typo.text[size].fontSize};
-    font-weight: ${weight ? getFontWeight(weight) : theme.typo.text[size].fontWeight};
-    line-height: ${theme.typo.text[size].lineHeight};
-  `};
-`;
 
 export const TextComponent = ({ size, weight, children }: TextProps) => {
   return (
     <>
-      <StyleText size={size} weight={weight}>
+      <S.StyleText size={size} weight={weight}>
         {children}
-      </StyleText>
+      </S.StyleText>
     </>
   );
 };
