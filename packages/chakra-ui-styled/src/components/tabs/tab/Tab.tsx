272b896b40ca0ac728f06ef363069d607fe27adf
smurf <smurf@kakao.com>
2024-04-16 22:24:23 +0900

    feat : tab component

--- packages/chakra-ui-styled/src/components/tabs/tab/Tab.tsx
+++ packages/chakra-ui-styled/src/components/tabs/tab/Tab.tsx
@@ -1,115 +1,69 @@
 import styled, { css } from 'styled-components';
+import { sizeType } from './tab.types';
 
-const BoxSM = styled.div`
-  ${({ theme }) => css`
-    padding: ${theme.spacings[4]} ${theme.spacings[1]};
+const sizeStyles = {
+  sm: css`
+    padding: ${({ theme }) => theme.spacings[4]} ${({ theme }) => theme.spacings[1]};
     background-color: transparent;
-  `}
-`;
-const BoxMD = styled.div`
-  ${({ theme }) => css`
-    padding: ${theme.spacings[4]} ${theme.spacings[2]};
+  `,
+  md: css`
+    padding: ${({ theme }) => theme.spacings[4]} ${({ theme }) => theme.spacings[2]};
     background-color: transparent;
-  `}
-`;
-const BoxLG = styled.div`
-  ${({ theme }) => css`
-    padding: ${theme.spacings[4]} ${theme.spacings[3]};
+  `,
+  lg: css`
+    padding: ${({ theme }) => theme.spacings[4]} ${({ theme }) => theme.spacings[3]};
     background-color: transparent;
-  `}
-`;
-const UnstyledSM = styled(BoxSM)`
-  border: none;
-  background-color: transparent;
-`;
-const UnstyledMD = styled(BoxMD)`
-  border: none;
-  background-color: transparent;
-`;
-const UnstyledLG = styled(BoxLG)`
-  border: none;
-  background-color: transparent;
-`;
-const LineSM = styled(BoxSM)`
-  ${({ theme }) => css`
-    border-bottom: 2px solid ${theme.color.blue[600]};
-    color: ${theme.color.blue[600]};
-  `}
-`;
-const LineMD = styled(BoxMD)`
-  ${({ theme }) => css`
-    border-bottom: 2px solid ${theme.color.blue[600]};
-    color: ${theme.color.blue[600]};
-  `}
-`;
-const LineLG = styled(BoxLG)`
-  ${({ theme }) => css`
-    border-bottom: 2px solid ${theme.color.blue[600]};
-    color: ${theme.color.blue[600]};
-  `}
-`;
+  `
+};
 
-const EnclosedSM = styled(BoxSM)`
-  ${({ theme }) => css`
-    border-radius: ${theme.radii.md} ${theme.radii.md} 0rem 0rem;
-    background-color: ${theme.color.white.white};
-    border-top: ${theme.spacings.px} solid ${theme.color.gray[200]};
-    border-left: ${theme.spacings.px} solid ${theme.color.gray[200]};
-    border-right: ${theme.spacings.px} solid ${theme.color.gray[200]};
-    color: ${theme.color.blue[600]};
-  `}
-`;
-const EnclossedMD = styled(BoxMD)`
-  ${({ theme }) => css`
-    border-radius: ${theme.radii.md} ${theme.radii.md} 0rem 0rem;
-    background-color: ${theme.color.white.white};
-    border-top: ${theme.spacings.px} solid ${theme.color.gray[200]};
-    border-left: ${theme.spacings.px} solid ${theme.color.gray[200]};
-    border-right: ${theme.spacings.px} solid ${theme.color.gray[200]};
-    color: ${theme.color.blue[600]};
-  `}
-`;
-const EnclosedLG = styled(BoxLG)`
-  ${({ theme }) => css`
-    border-radius: ${theme.radii.md} ${theme.radii.md} 0rem 0rem;
-    background-color: ${theme.color.white.white};
-    border-top: ${theme.spacings.px} solid ${theme.color.gray[200]};
-    border-left: ${theme.spacings.px} solid ${theme.color.gray[200]};
-    border-right: ${theme.spacings.px} solid ${theme.color.gray[200]};
-    color: ${theme.color.blue[600]};
-  `}
-`;
+const styles = {
+  unstyled: css`
+    border: none;
+    background-color: transparent;
+  `,
+  line: css`
+    border-bottom: 2px solid ${({ theme }) => theme.color.blue[600]};
+    color: ${({ theme }) => theme.color.blue[600]};
+  `,
+  enclosed: css`
+    border-radius: ${({ theme }) => theme.radii.md} ${({ theme }) => theme.radii.md} 0rem 0rem;
+    background-color: ${({ theme }) => theme.color.white.white};
+    border-top: ${({ theme }) => theme.spacings.px} solid ${({ theme }) => theme.color.gray[200]};
+    border-left: ${({ theme }) => theme.spacings.px} solid ${({ theme }) => theme.color.gray[200]};
+    border-right: ${({ theme }) => theme.spacings.px} solid ${({ theme }) => theme.color.gray[200]};
+    color: ${({ theme }) => theme.color.blue[600]};
+  `,
+  softRounded: css`
+    border-radius: 999px;
+    background-color: ${({ theme }) => theme.color.blue[100]};
+    color: ${({ theme }) => theme.color.blue[700]};
+  `,
+  solidRounded: css`
+    border-radius: 999px;
+    background-color: ${({ theme }) => theme.color.blue[600]};
+    color: ${({ theme }) => theme.color.white.white};
+  `
+};
 
-const SoftRoundedSM = styled(BoxSM)`
-${({ theme }) => css`
-  border-radius: 9999px;
-  background-color: ${theme.color.blue[100]};
-  color: ${theme.color.blue[700]};
-`}}
+const TabBtn = styled.div<{ size: sizeType; style: keyof typeof styles }>`
+  ${({ size }) => sizeStyles[size]}
+  ${({ style }) => styles[style]}
 `;
-
-const SolidRoundedSM = styled(BoxSM)`
-${({ theme }) => css`
-  border-radius: 9999px;
-  background-color: ${theme.color.blue[600]};
-  color: ${theme.color.white.white};
-`}}`;
 
 // TODO
 // size = sm md lg  프롭스로 전달 후
 // {({size))=> size === 'sm' && } 식으로 작업하여 동적 작업하기.
-const Tab = () => {
+const Tab = ({ size, style }: { size: sizeType; style: keyof typeof styles }) => {
   //rounded = fsz  semibold 그외 medium
 
   const text = 'Tabs';
   return (
     <>
       <div>
-        <UnstyledSM>{text}</UnstyledSM>
-        <LineSM>{text}</LineSM>
-        <EnclosedSM>{text}</EnclosedSM>
-        <SoftRoundedSM>{text}</SoftRoundedSM>
-        <SolidRoundedSM>{text}</SolidRoundedSM>
+        {/*<TabBtn size="sm" style="unstyled">*/}
+        <TabBtn size={size} style={style}>
+          {text}
+        </TabBtn>
       </div>
       <div></div>
     </>
---
+++ packages/chakra-ui-styled/src/components/tabs/tab/tab.types.ts
@@ -0,0 +1,1 @@
+export type sizeType = 'sm' | 'md' | 'lg';
