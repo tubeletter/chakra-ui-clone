9a74493a00486f70b2121cf7614b39df96c7d050
gayun.kim <gayun.kim@lotte.net>
2024-04-18 23:23:24 +0900

    fix : tab fix

--- packages/chakra-ui-styled/src/components/tabs/tab/Tab.tsx
+++ packages/chakra-ui-styled/src/components/tabs/tab/Tab.tsx
@@ -3,66 +3,64 @@ import { tabType } from './tab.types';
 
 export const sizeStyles = {
   sm: css`
-    padding: ${({ theme }) => theme.spacings[4]} ${({ theme }) => theme.spacings[1]};
+    padding: ${({ theme }) => theme.spacing[4]} ${({ theme }) => theme.spacing[1]};
     background-color: transparent;
   `,
   md: css`
-    padding: ${({ theme }) => theme.spacings[4]} ${({ theme }) => theme.spacings[2]};
+    padding: ${({ theme }) => theme.spacing[4]} ${({ theme }) => theme.spacing[2]};
     background-color: transparent;
   `,
   lg: css`
-    padding: ${({ theme }) => theme.spacings[4]} ${({ theme }) => theme.spacings[3]};
+    padding: ${({ theme }) => theme.spacing[4]} ${({ theme }) => theme.spacing[3]};
     background-color: transparent;
   `
 };
 
-export const styles = {
+export const tabStyle = {
   unstyled: css`
-    border: none;
     background-color: transparent;
+    border: none;
   `,
   line: css`
-    border-bottom: 2px solid ${({ theme }) => theme.color.blue[600]};
     color: ${({ theme }) => theme.color.blue[600]};
+    border-bottom: 2px solid ${({ theme }) => theme.color.blue[600]};
   `,
   enclosed: css`
-    border-radius: ${({ theme }) => theme.radii.md} ${({ theme }) => theme.radii.md} 0rem 0rem;
-    background-color: ${({ theme }) => theme.color.white.white};
-    border-top: ${({ theme }) => theme.spacings.px} solid ${({ theme }) => theme.color.gray[200]};
-    border-left: ${({ theme }) => theme.spacings.px} solid ${({ theme }) => theme.color.gray[200]};
-    border-right: ${({ theme }) => theme.spacings.px} solid ${({ theme }) => theme.color.gray[200]};
     color: ${({ theme }) => theme.color.blue[600]};
+    background-color: ${({ theme }) => theme.color.white.white};
+    border-top: ${({ theme }) => theme.spacing.px} solid ${({ theme }) => theme.color.gray[200]};
+    border-right: ${({ theme }) => theme.spacing.px} solid ${({ theme }) => theme.color.gray[200]};
+    border-left: ${({ theme }) => theme.spacing.px} solid ${({ theme }) => theme.color.gray[200]};
+    border-radius: ${({ theme }) => theme.radii.md} ${({ theme }) => theme.radii.md} 0 0;
   `,
   softRounded: css`
-    border-radius: 999px;
-    background-color: ${({ theme }) => theme.color.blue[100]};
     color: ${({ theme }) => theme.color.blue[700]};
+    background-color: ${({ theme }) => theme.color.blue[100]};
+    border-radius: 999px;
   `,
   solidRounded: css`
-    border-radius: 999px;
-    background-color: ${({ theme }) => theme.color.blue[600]};
     color: ${({ theme }) => theme.color.white.white};
+    background-color: ${({ theme }) => theme.color.blue[600]};
+    border-radius: 999px;
   `
 };
 
 const TabBtn = styled.div<tabType>`
-  if(active) {
-    ${({ style }) => styles[style]}
-  }
+  ${({ active, styleType }) => active && tabStyle[styleType]}
   ${({ size }) => sizeStyles[size]}
 `;
 
 // TODO
 // size = sm md lg  프롭스로 전달 후
 // {({size))=> size === 'sm' && } 식으로 작업하여 동적 작업하기.
-const Tab = ({ size, style, active, text = 'Tabs' }: tabType) => {
+const Tab = ({ size, styleType, active, text = 'Tabs' }: tabType) => {
   //rounded = fsz  semibold 그외 medium
 
   return (
     <>
       <div>
         {/*<TabBtn size="sm" style="unstyled">*/}
-        <TabBtn size={size} style={style} active={active}>
+        <TabBtn size={size} styleType={styleType} active={active}>
           {text}
         </TabBtn>
       </div>
