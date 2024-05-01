51aa1651782c2b14d7a8587ff71bf037c68346a3
bboing <gayoon109@gmail.com>
2024-05-02 08:30:23 +0900

    fix : typo set add

--- packages/chakra-ui-styled/src/foundation/typography/Text.styled.ts
+++ packages/chakra-ui-styled/src/foundation/typography/Text.styled.ts
@@ -2,10 +2,10 @@ import styled, { css } from 'styled-components';
 import { TextProps } from './Text';
 
 export const StyleText = styled.p<TextProps>`
-  ${({ theme, size, weight }) => css`
+  ${({ theme, size, weight, lineHeight }) => css`
     ${theme.typo.fontFamily};
     font-size: ${theme.typo.text[size].fontSize};
     font-weight: ${weight ? theme.typo.fontWeight[weight] : theme.typo.text[size].fontWeight};
-    line-height: ${theme.typo.text[size].lineHeight};
+    line-height: ${lineHeight ? theme.typo.lineHeight[lineHeight] : theme.typo.text[size].lineHeight};
   `};
 `;
